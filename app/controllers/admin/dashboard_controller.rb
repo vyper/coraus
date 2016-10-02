801453a40038279fb29180fefe0dcb71b4659eb5
Leonardo Saraiva <vyper@maneh.org>
2016-10-02 19:35:13 -0300

    Add talkers waiting for the sessions start on dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,8 @@
 module Admin
   class DashboardController < ApplicationController
     def index
-      @talkers_without_schedule = Talker.without_schedule
+      @talkers_without_schedule    = Talker.without_schedule
+      @talkers_waiting_for_session = Talker.waiting_for_session
     end
   end
 end
