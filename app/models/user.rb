5b207a957185da2f470dcee1267b73ff8b3c869f
Leonardo Saraiva <vyper@maneh.org>
2016-10-02 15:41:09 -0300

    Add authentication (using devise)

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :registerable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :recoverable, :rememberable,
+         :trackable, :validatable
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   resources :talkers, only: [:new, :create, :show]
+
+  root to: 'talkers#new'
 end
