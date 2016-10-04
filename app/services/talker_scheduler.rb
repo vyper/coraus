7236db23b6cb90c00b260a492a89965d10e7b188
Leonardo Saraiva <vyper@maneh.org>
2016-10-03 22:38:48 -0300

    Add notification when schedule was confirmed

--- app/services/talker_scheduler.rb
+++ app/services/talker_scheduler.rb
@@ -8,5 +8,34 @@ class TalkerScheduler
 
   def call
     schedule.update(talker: talker)
+
+    notify_talker
+    notify_listener
+
+    true
+  end
+
+  private
+
+  def notify_talker
+    mail = SendgridBaseMailer.new(
+      template_id: '77bb51d5-6871-4622-825d-d32e0074a301',
+      to: { name: talker.name, email: talker.email },
+      from: { name: 'Coraus', email: 'contato@coraus.com.br' },
+      params: { '-name-' => talker.name, '-listener-' => schedule.listener.name, '-scheduled_to-' => schedule.scheduled_to, '-room_url-' => schedule.room_url }
+    )
+
+    mail.deliver
+  end
+
+  def notify_listener
+    mail = SendgridBaseMailer.new(
+      template_id: '2babd711-4791-404d-a49f-9fffec5b88c8',
+      to: { name: schedule.listener.name, email: schedule.listener.email },
+      from: { name: 'Coraus', email: 'contato@coraus.com.br' },
+      params: { '-name-' => schedule.listener.name, '-talker-' => talker.name, '-scheduled_to-' => schedule.scheduled_to, '-room_url-' => schedule.room_url }
+    )
+
+    mail.deliver
   end
 end
