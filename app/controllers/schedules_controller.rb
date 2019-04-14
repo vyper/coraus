0bcceed2ce80492531c8e835d2280eaaa1a3b18b
Leonardo Saraiva <vyper@maneh.org>
2019-04-14 11:48:46 -0300

    ordering available schedules

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,6 +1,6 @@
 class SchedulesController < ApplicationController
   def index
-    @schedules = Schedule.where(scheduled_to: Date.tomorrow..7.days.from_now).available(params[:gender])
+    @schedules = Schedule.order(scheduled_to: :asc) #.where(scheduled_to: Date.tomorrow..7.days.from_now).available(params[:gender])
 
     respond_to do |format|
       format.json { render json: @schedules }
