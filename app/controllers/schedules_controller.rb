f4ab09cca6a0557a64f5171c1a086344c3e6726b
Leonardo Saraiva <vyper@maneh.org>
2019-04-17 09:13:21 -0300

    Increase from 7 to 10 days from now

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,6 +1,6 @@
 class SchedulesController < ApplicationController
   def index
-    @schedules = Schedule.where(scheduled_to: Date.tomorrow..7.days.from_now).available(params[:gender]).order(scheduled_to: :asc)
+    @schedules = Schedule.where(scheduled_to: Date.tomorrow..10.days.from_now).available(params[:gender]).order(scheduled_to: :asc)
 
     respond_to do |format|
       format.json { render json: @schedules }
