06a87e9a451c69d9c448c76e726bcca67c532f3d
Leonardo Saraiva <vyper@maneh.org>
2019-04-27 22:39:31 -0300

    Revert "Fix id for 'Finished-schedule-to-listener' email"
    This reverts commit 68574359a57268e0428d23f80144c75cb1d0c45f.

--- app/services/schedule_finisher.rb
+++ app/services/schedule_finisher.rb
@@ -19,7 +19,7 @@ class ScheduleFinisher
 
   def notify_listener
     mail = SendgridBaseMailer.new(
-      template_id: 'a51cd01a-d111-4bac-aa2c-78d0e4a4b869',
+      template_id: '9744401c-9cce-41c1-8150-0d5145d73c09',
       to: { name: schedule.listener.name, email: schedule.listener.email },
       from: { name: 'Athento', email: 'contato@athento.com.br' },
       params: { '-name-' => schedule.listener.name, '-talker-' => schedule.talker.name, '-scheduled_to-' => I18n.l(schedule.scheduled_to, format: :short) }
