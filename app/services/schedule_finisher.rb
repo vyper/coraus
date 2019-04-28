3e905b0cd44cdd1391155a508d4b3e1bc62ac7a3
Leonardo Saraiva <vyper@maneh.org>
2019-04-27 22:12:47 -0300

    Add email to listener on schedule finish

--- app/services/schedule_finisher.rb
+++ app/services/schedule_finisher.rb
@@ -9,12 +9,24 @@ class ScheduleFinisher
   def call
     schedule.update(params)
 
+    notify_listener
     notify_talker
 
     true
   end
 
   private
+
+  def notify_listener
+    mail = SendgridBaseMailer.new(
+      template_id: '9744401c-9cce-41c1-8150-0d5145d73c09',
+      to: { name: schedule.listener.name, email: schedule.listener.email },
+      from: { name: 'Athento', email: 'contato@athento.com.br' },
+      params: { '-name-' => schedule.listener.name, '-talker-' => schedule.talker.name, '-scheduled_to-' => I18n.l(schedule.scheduled_to, format: :short) }
+    )
+
+    mail.deliver
+  end
 
   def notify_talker
     mail = SendgridBaseMailer.new(
