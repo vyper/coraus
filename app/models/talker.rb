1e0f5bace1bde06e88a6b556875cc7a2e565f51c
Leonardo Saraiva <vyper@maneh.org>
2020-04-07 13:15:58 -0300

    Remove presence of talker phone

--- app/models/talker.rb
+++ app/models/talker.rb
@@ -1,7 +1,6 @@
 class Talker < ApplicationRecord
   validates :name,                      presence: true
   validates :email,                     presence: true
-  validates :phone,                     presence: true
   validates :preferred_listener_gender, presence: true
 
   has_one :schedule, required: false
